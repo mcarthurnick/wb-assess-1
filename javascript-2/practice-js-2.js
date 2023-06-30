6465e7ba90636d9fb6486c52d2428f55181e3caf
Nick McArthur <mcarthurnick@gmail.com>
2023-06-30 13:48:41 -0600

    finished JavaScript 2 assessment and uploaded screenshot of passed tests.

--- javascript-2/practice-js-2.js
+++ javascript-2/practice-js-2.js
@@ -9,7 +9,7 @@
     It should be a string containing your best definition of what Git is.
 */
 
-//CODE HERE
+let gitDefinition = "Git is a software that helps us with version control for our code."
 
 //////////////////PROBLEM 2////////////////////
 /*
@@ -17,7 +17,7 @@
     It should be a string containing your best definition of what GitHub is.
 */
 
-//CODE HERE
+let gitHubDefinition = "Github allows us to store code in repositories so that developers can track progress and changes made."
 
 //////////////////PROBLEMS 3 - 9////////////////////
 /*
@@ -33,7 +33,10 @@
     following the guidelines above to describe the init command.
 */
 
-//CODE HERE
+const init = {
+    description: 'This initializes the folder so that it will begin tracking changes made to files.',
+    code: 'git init'
+}
 
 //////////////////PROBLEM 4////////////////////
 /*
@@ -41,7 +44,10 @@
     following the guidelines above to describe the clone command.
 */
 
-//CODE HERE
+const clone = {
+    description: 'git clone takes a repository that has already been made and makes a copy of it.',
+    code: 'git clone'
+}
 
 //////////////////PROBLEM 5////////////////////
 /*
@@ -49,7 +55,10 @@
     following the guidelines above to describe the status command.
 */
 
-//CODE HERE
+const status = {
+    description: 'git status allows us to see the current working status of our work. It will show if there are files that needed to be added in order to commit etc.',
+    code: 'git status'
+}
 
 //////////////////PROBLEM 6////////////////////
 /*
@@ -57,7 +66,10 @@
     following the guidelines above to describe the add command.
 */
 
-//CODE HERE
+const add = {
+    description: 'git add will add the changes that were made in the files so they are ready to commit.',
+    code: 'git add .'
+}
 
 //////////////////PROBLEM 7////////////////////
 /*
@@ -65,7 +77,10 @@
     following the guidelines above to describe the commit command.
 */
 
-//CODE HERE
+const commit = {
+    description: 'git commit allows you to make a checkpoint for the code and lets you send a message so you can explain what changes were made.',
+    code: 'git commit -m ""'
+}
 
 //////////////////PROBLEM 8////////////////////
 /*
@@ -73,7 +88,10 @@
     following the guidelines above to describe the command to add a remote location to your git repository.
 */
 
-//CODE HERE
+const addRemote = {
+    description: 'git remote add origin creates a link between our repository in github and our local repository so we can push code to github.',
+    code: 'git remote add origin'
+}
 
 //////////////////PROBLEM 9////////////////////
 /*
@@ -81,4 +99,7 @@
     following the guidelines above to describe the push command.
 */
 
-//CODE HERE
+const push = {
+    description: 'git push takes all of our commits that we have made and pushes them into our github repository.',
+    code: 'git push'
+}
